b65874b1f958688233685107fbe023cc4667b21a
Kendra Dunham and Sadio Ali <Guest@epicodus-117.local>
2016-05-25 16:44:15 -0700

    this is broken and nothing works and life is meaningless

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -2,6 +2,11 @@ var Dino = require('./../js/dino.js').Dino;
 
 
 $(document).ready(function(){
-  var dino = new Dino("name");
-  dino.get();
+  debugger;
+  var dino = new Dino();
+  dino.grab();
+  dino.slice();
+  // var sliceTheDino = getTheDino.split("");
+  // $('#dinosaur').text(dino.grab());
+  console.log(nameTheDino);
 });
--- js/dino.js
+++ js/dino.js
@@ -1,12 +1,28 @@
-exports.Dino = function(dinoName) {
-  this.dinoName = dinoName;
+exports.Dino = function() {
+  this.dinoName = "";
+  this.slicedName = "";
+  this.jumbledName = "";
 };
 
-exports.Dino.prototype.get = function() {
+exports.Dino.prototype.grab = function() {
   $.get('https://dinoipsum.herokuapp.com/api/?format=html&words=1&paragraphs=1').then(function(response) {
-    var slicedDino = response.slice(3,-5);
-    console.log(slicedDino);
+    debugger;
+    this.dinoName = response;
+    return this.dinoName;
   }).fail(function(error){
     console.log("The dinosaur has died");
   });
 };
+
+exports.Dino.prototype.slice = function() {
+  this.slicedName = this.dinoName.slice();
+  return this.slicedName;
+  console.log(this.slicedName);
+}
+
+// exports.Dino.prototype.jumble = function() {
+//   var dinoName = this.grab();
+//   var sliceTheDino = dinoName.toUpper();
+//   console.log(sliceTheDino);
+//   // return sliceTheDino;
+// };
