5b9ae2869e7ccedd31da4c0b01733c7af022c4fc
Kendra Dunham and Sadio Ali <kendra.dunham@gmail.com>
2016-05-26 09:48:54 -0700

    it works again\!

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,12 +1,33 @@
 var Dino = require('./../js/dino.js').Dino;
 
+var display = function(name) {
+  console.log(name + " is the name");
+  return name;
+};
+
+var splitName = function(name) {
+  var splittedName = name.split("");
+  console.log(splittedName + " is the splitted name");
+  return splittedName;
+};
+
+var jumble = function(splittedName) {
+
+  var length = splittedName.length;
+  for(var i = length -1; i >0; i--){
+    var j = Math.floor(Math.random() * (i+1));
+    var jumbledName = splittedName[i];
+    splittedName[i] = splittedName[j];
+    splittedName[j] = jumbledName;
+  }
+  var jumbledName = splittedName.join("");
+  return jumbledName;
+};
 
 $(document).ready(function(){
-  debugger;
   var dino = new Dino();
-  dino.grab();
-  dino.slice();
+  dino.grab(display, splitName, jumble);
+  // var jumbled = name.jumble();
   // var sliceTheDino = getTheDino.split("");
   // $('#dinosaur').text(dino.grab());
-  console.log(nameTheDino);
 });
--- js/dino.js
+++ js/dino.js
@@ -1,28 +1,30 @@
 exports.Dino = function() {
   this.dinoName = "";
-  this.slicedName = "";
-  this.jumbledName = "";
 };
 
-exports.Dino.prototype.grab = function() {
+// var sortStuff = function(data) {
+//   console.log(data);
+// };
+// var display = function(data) {
+//   console.log(data);
+// };
+
+exports.Dino.prototype.grab = function(display, splitName, jumble) {
   $.get('https://dinoipsum.herokuapp.com/api/?format=html&words=1&paragraphs=1').then(function(response) {
-    debugger;
     this.dinoName = response;
-    return this.dinoName;
+    var slicedName = this.dinoName.slice(3, -5);
+    var displayReturn = display(slicedName);
+    var splitReturn = splitName(displayReturn);
+    var jumbleReturn = jumble(splitReturn);
   }).fail(function(error){
     console.log("The dinosaur has died");
   });
 };
 
-exports.Dino.prototype.slice = function() {
-  this.slicedName = this.dinoName.slice();
-  return this.slicedName;
-  console.log(this.slicedName);
-}
-
 // exports.Dino.prototype.jumble = function() {
-//   var dinoName = this.grab();
-//   var sliceTheDino = dinoName.toUpper();
+//   var dinoName = this.dinoName;
+//   var slicedName = dinoName.grab();
+//   var sliceTheDino = slicedName.split("");
 //   console.log(sliceTheDino);
 //   // return sliceTheDino;
 // };
